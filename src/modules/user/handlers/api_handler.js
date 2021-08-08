10280dd792900901cf92cb02a4ec51822e5a1151
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-08 11:26:36 +0700

    create api handler for get list user

--- src/modules/user/handlers/api_handler.js
+++ src/modules/user/handlers/api_handler.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const queryDomain = require('../repositories/queries/domain');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.json({
-    message: 'module user 🌏'
-  });
+// handler list user
+router.get('/', async (req, res) => {
+  const getRequest = async () => queryDomain.getUsers();
+  const sendResponse = async (result) => {
+    (result.err) ? res.json({
+      status: false,
+      message: 'list user',
+      code: 400,
+      data: result
+    }) : res.json({
+      status: true,
+      message: 'list user',
+      code: 200,
+      data: result
+    });
+  };
+  sendResponse(await getRequest());
 });
 
 router.get('/listuser', (req, res) => {
