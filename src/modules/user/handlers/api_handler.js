1a1e4ee166a77d6c49699e78b318158800a5e4c7
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-08 11:39:18 +0700

    create api handler for get one user

--- src/modules/user/handlers/api_handler.js
+++ src/modules/user/handlers/api_handler.js
@@ -5,27 +5,42 @@ const router = express.Router();
 
 // handler list user
 router.get('/', async (req, res) => {
-  const getRequest = async () => queryDomain.getUsers();
+  const getUsers = async () => queryDomain.getUsers();
   const sendResponse = async (result) => {
     (result.err) ? res.json({
       status: false,
-      message: 'list user',
+      message: 'can not get list user',
       code: 400,
       data: result
     }) : res.json({
       status: true,
-      message: 'list user',
+      message: 'success get list user',
       code: 200,
       data: result
     });
   };
-  sendResponse(await getRequest());
+  sendResponse(await getUsers());
 });
 
-router.get('/listuser', (req, res) => {
-  res.json({
-    message: 'list user 🌏'
-  });
+// handler get one user
+router.get('/:id', async (req, res) => {
+  const userId = req.params.id;
+
+  const getOneUser = async () => queryDomain.getOneUser(userId);
+  const sendResponse = async (result) => {
+    (result.err) ? res.json({
+      status: false,
+      message: `can not get user with id ${userId}`,
+      code: 404,
+      data: result
+    }) : res.json({
+      status: true,
+      message: `success get user with id ${userId}`,
+      code: 200,
+      data: result
+    });
+  };
+  sendResponse(await getOneUser());
 });
 
 module.exports = router;
