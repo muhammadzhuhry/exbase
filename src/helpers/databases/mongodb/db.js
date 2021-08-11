32457162e8543e9f791c465f43ac4ecf3a4d685f
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-11 16:55:12 +0700

    add find one

--- src/helpers/databases/mongodb/db.js
+++ src/helpers/databases/mongodb/db.js
@@ -17,4 +17,28 @@ class DB {
     const dbName = pattern.exec(config);
     return dbName[1];
   }
+
+  async findOne(parameter) {
+    const dbName = await this.getDatabase();
+    console.log(dbName)
+    const result = await mongoConnection.getConnection(this.config);
+    if (result.err) {
+      console.log('error mongodb connection');
+      return result;
+    }
+    try {
+      const cacheConnection = result.data.db;
+      const connection = cacheConnection.db(dbName);
+      const db = connection.collection(this.collectionName);
+      const recordset = await db.findOne(parameter);
+      if (validate.isEmpty(recordset)) {
+        return wrapper.error('data not found please try another input');
+      }
+      return wrapper.data(recordset);
+
+    } catch (err) {
+      console.log('error find data in mongodb');
+      return wrapper.error(`error find one mongo ${err.message}`);
+    }
+  }
 }
