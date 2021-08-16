087426cee18b3fc6c23cea6494f74b5383db4012
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-16 21:17:26 +0700

    add function check error code

--- src/helpers/utils/wrapper.js
+++ src/helpers/utils/wrapper.js
@@ -1,3 +1,6 @@
+const { NotFoundError, InternalServerError, BadRequestError, ConflictError,
+  ForbiddenError, UnauthorizedError } = require('../error');
+
 const data = (data) => ({ error: null, data});
 
 const error = (error) => ({ error, data: null });
@@ -10,11 +13,12 @@ const response = (res, type, result, message = '', responseCode = 200) => {
  code = responseCode;
 
  if (type === 'fail') {
+   const errCode = checkErrorCode(result.err);
    status = false;
    data = result.error.data || '';
    message = result.error.message || message;
    code = result.error.code
-   responseCode = 0;
+   responseCode = errCode;
  }
 
  let modelResponse = {
@@ -27,7 +31,27 @@ const response = (res, type, result, message = '', responseCode = 200) => {
  res.responseCode(responseCode, modelResponse);
 }
 
+const checkErrorCode = (error) => {
+  switch (error.constructor) {
+  case BadRequestError:
+    return httpError.BAD_REQUEST;
+  case ConflictError:
+    return httpError.CONFLICT;
+  case ForbiddenError:
+    return httpError.FORBIDDEN;
+  case InternalServerError:
+    return httpError.INTERNAL_ERROR;
+  case NotFoundError:
+    return httpError.NOT_FOUND;
+  case UnauthorizedError:
+    return httpError.UNAUTHORIZED;
+  default:
+    return httpError.CONFLICT;
+  }
+};
+
 module.exports = {
   data,
-  error
+  error,
+  response
 };
