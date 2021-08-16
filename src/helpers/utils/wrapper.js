a9a8dc48a10901a55f703c7d9244ce28cea8fa98
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-16 21:15:30 +0700

    create function wrapper response

--- src/helpers/utils/wrapper.js
+++ src/helpers/utils/wrapper.js
@@ -2,6 +2,31 @@ const data = (data) => ({ error: null, data});
 
 const error = (error) => ({ error, data: null });
 
+const response = (res, type, result, message = '', responseCode = 200) => {
+ let status, data, code;
+ 
+ status = true;
+ data = result.data;
+ code = responseCode;
+
+ if (type === 'fail') {
+   status = false;
+   data = result.error.data || '';
+   message = result.error.message || message;
+   code = result.error.code
+   responseCode = 0;
+ }
+
+ let modelResponse = {
+  success: status,
+  data,
+  message,
+  code
+ };
+
+ res.responseCode(responseCode, modelResponse);
+}
+
 module.exports = {
   data,
   error
