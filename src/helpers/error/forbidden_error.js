1d043f087c45109bfcf6c5ec1682bb1e192371ce
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-17 17:56:33 +0700

    modify forbidden error

--- src/helpers/error/forbidden_error.js
+++ src/helpers/error/forbidden_error.js
@@ -1,8 +1,10 @@
+const { ERROR:httpError } = require('../http-status/status-code');
+
 class ForbiddenError {
-  constructor(param = 'forbidden') {
+  constructor(param = 'forbidden', data) {
     this.message = param.message || param;
-    this.data = param.data;
-    this.code = param.code;
+    this.data = data;
+    this.code = httpError.FORBIDDEN;
   }
 }
 
