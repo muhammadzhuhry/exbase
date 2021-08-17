c17158118903b605a09db016e0cb351503cfb07f
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-17 17:56:53 +0700

    modify not found error

--- src/helpers/error/not_found_error.js
+++ src/helpers/error/not_found_error.js
@@ -1,8 +1,10 @@
+const { ERROR:httpError } = require('../http-status/status-code');
+
 class NotFoundError {
-  constructor(param = 'not found') {
+  constructor(param = 'not found', data) {
     this.message = param.message || param;
-    this.data = param.data;
-    this.code = param.code;
+    this.data = data;
+    this.code = httpError.NOT_FOUND;
   }
 }
 
