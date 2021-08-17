8340897af9aefa72be594fd7e2c4c533cc01a75c
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-17 17:56:16 +0700

    modify bad request error

--- src/helpers/error/bad_request_error.js
+++ src/helpers/error/bad_request_error.js
@@ -1,8 +1,10 @@
+const { ERROR:httpError } = require('../http-status/status-code');
+
 class BadRequestError {
-  constructor(param = 'bad request') {
+  constructor(param = 'bad request', data) {
     this.message = param.message || param;
-    this.data = param.data;
-    this.code = param.code;
+    this.data = data;
+    this.code = httpError.BAD_REQUEST;
   }
 }
 
