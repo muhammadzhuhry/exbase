bc22f4721f6aa42bbf9303d67ce66c52ca2438c3
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-19 20:03:38 +0700

    change default port

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const app = require('./src/app/server');
 const config = require('./src/global_config');
 const logger = require('./src/helpers/utils/logger');
 
-const port = config.get('/port') || 1900;
+const port = config.get('/port') || 8000;
 app.listen(port, () => {
   const ctx = 'app-listen';
-  logger.log(ctx, `${config.get('/name')}-server started, listening at ${config.get('/baseUrl')}:${port}`, 
+  logger.log(ctx, `${config.get('/name')}-server started, listening at ${config.get('/baseUrl')}:${port}`,
     'initiate application');
 });
