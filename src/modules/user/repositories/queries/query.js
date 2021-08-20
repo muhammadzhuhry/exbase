ded2255adf7bbd437ac1ef51405781f85ce7918e
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-20 21:33:04 +0700

    modify query count data

--- src/modules/user/repositories/queries/query.js
+++ src/modules/user/repositories/queries/query.js
@@ -3,15 +3,21 @@ class Query {
     this.db = db;
   }
 
+  async findOneUser(parameter) {
+    this.db.setCollection('users');
+    const recordset = await this.db.findOne(parameter);
+    return recordset;
+  }
+
   async findUsers(fieldName, row, page, parameter) {
     this.db.setCollection('users');
     const recordset = await this.db.findMany(fieldName, row, page, parameter);
     return recordset;
   }
 
-  async findOneUser(parameter) {
+  async countUsers() {
     this.db.setCollection('users');
-    const recordset = await this.db.findOne(parameter);
+    const recordset = await this.db.countData();
     return recordset;
   }
 }
