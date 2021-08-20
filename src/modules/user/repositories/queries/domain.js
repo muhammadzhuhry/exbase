58d6f40aacb1f85b6c7e2b0c626bdd551d3bbb13
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-20 21:33:29 +0700

    return get users with pagination

--- src/modules/user/repositories/queries/domain.js
+++ src/modules/user/repositories/queries/domain.js
@@ -4,19 +4,10 @@ const Model = require('./query_model');
 const config = require('../../../../global_config');
 const Mongo = require('../../../../helpers/databases/mongodb/db');
 const wrapper = require('../../../../helpers/utils/wrapper');
-const { NotFoundError } = require('../../../../helpers/error');
+const { NotFoundError, BadRequestError } = require('../../../../helpers/error');
 
 const mongodb = new Mongo(config.get('/mongodb').url);
 const query = new Query(mongodb);
-
-const getUsers = async () => {
-  const user = await query.findUsers({});
-  if (validate.isEmpty(user.data)) {
-    return wrapper.error(new NotFoundError('can not find user', []));
-  }
-
-  return wrapper.data(user.data);
-};
 
 const getOneUser = async (data) => {
   let userId, result;
@@ -25,7 +16,7 @@ const getOneUser = async (data) => {
 
   const user = await query.findOneUser({ id: userId });
   if (validate.isEmpty(user.data)) {
-    return wrapper.error(new NotFoundError('can not find user', {}));
+    return wrapper.error(new NotFoundError(`can not find user with id ${userId}`, {}));
   }
 
   result = Model.user();
@@ -36,7 +27,34 @@ const getOneUser = async (data) => {
   return wrapper.data(result);
 };
 
+const getUsers = async (data) => {
+  let page, size, metadata;
+
+  if (data.page == 0) {
+    return wrapper.error(new BadRequestError('page must start from 1', []));
+  }
+
+  page = parseInt(data.page) || 1;
+  size = parseInt(data.size) || 10;
+
+  const users = await query.findUsers({}, size, page);
+  if (validate.isEmpty(users.data)) {
+    return wrapper.error(new NotFoundError('can not find user', []));
+  }
+
+  const countUsers = await query.countUsers();
+
+  metadata = {
+    page,
+    size: users.data.length,
+    totalPage: Math.ceil(countUsers.data / size),
+    totalData: countUsers.data
+  };
+
+  return wrapper.paginationData(users.data, metadata);
+};
+
 module.exports = {
-  getUsers,
-  getOneUser
+  getOneUser,
+  getUsers
 };
