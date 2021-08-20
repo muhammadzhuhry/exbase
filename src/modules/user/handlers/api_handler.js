dea387f0cc3fd362eb3a82ce6df46831cf299956
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-20 21:32:53 +0700

    modify handler get listuser

--- src/modules/user/handlers/api_handler.js
+++ src/modules/user/handlers/api_handler.js
@@ -5,26 +5,28 @@ const { ERROR:httpError, SUCCESS:http } = require('../../../helpers/http-status/
 
 const router = express.Router();
 
-// handler list user
-router.get('/', async (req, res) => {
-  const getUsers = async () => queriesDomain.getUsers();
-  const sendResponse = async (result) => {
-    (result.error) ? wrapper.response(res, 'fail', result, 'can not get list user', httpError.NOT_FOUND)
-      : wrapper.response(res, 'success', result, 'success get list user', http.OK);
-  };
-  sendResponse(await getUsers());
-});
-
 // handler get one user
 router.get('/:id', async (req, res) => {
   const userId = req.params.id;
 
   const getOneUser = async () => queriesDomain.getOneUser(userId);
   const sendResponse = async (result) => {
-    (result.error) ? wrapper.response(res, 'fail', result, `can not get user with id ${userId}`, httpError.NOT_FOUND)
+    (result.error) ? wrapper.response(res, 'fail', result, result.error.message, httpError.NOT_FOUND)
       : wrapper.response(res, 'success', result, `success get user with id ${userId}`, http.OK);
   };
   sendResponse(await getOneUser());
+});
+
+// handler list user
+router.get('/', async (req, res) => {
+  const payload = { ...req.query };
+
+  const getUsers = async () => queriesDomain.getUsers(payload);
+  const sendResponse = async (result) => {
+    (result.error) ? wrapper.response(res, 'fail', result, result.error.message, httpError.NOT_FOUND)
+      : wrapper.paginationResponse(res, 'success', result, 'success get list user', http.OK);
+  };
+  sendResponse(await getUsers());
 });
 
 module.exports = router;
