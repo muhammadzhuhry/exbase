e3bed9f0cb04ddcf2c79a74e6cbed5d529020b93
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-21 13:37:25 +0700

    create query update user

--- src/modules/user/repositories/commands/command.js
+++ src/modules/user/repositories/commands/command.js
@@ -8,6 +8,13 @@ class Query {
     const recordset = await this.db.insertOne(document);
     return recordset;
   }
+
+  async updateUser(parameter, document){
+    this.db.setCollection('users');
+    const updateData = {$set : document};
+    const result = await this.db.upsertOne(parameter, updateData);
+    return result;
+  }
 }
 
 module.exports = Query;
