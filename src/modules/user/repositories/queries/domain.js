f2f85ea7ecdf2bc7ff8541e8496b25a9b9a773c3
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-08-21 12:22:59 +0700

    refactor logic model user

--- src/modules/user/repositories/queries/domain.js
+++ src/modules/user/repositories/queries/domain.js
@@ -20,15 +20,17 @@ const getOneUser = async (data) => {
   }
 
   result = Model.user();
-  result.userId = user.data.id;
+  result.id = user.data.id;
   result.name = user.data.name;
   result.gender = user.data.gender;
+  result.createdAt = user.data.createdAt;
+  result.updatedAt = user.data.updatedAt;
 
   return wrapper.data(result);
 };
 
 const getUsers = async (data) => {
-  let page, size, metadata;
+  let page, size, result = [], metadata;
 
   if (data.page == 0) {
     return wrapper.error(new BadRequestError('page must start from 1', []));
@@ -42,16 +44,27 @@ const getUsers = async (data) => {
     return wrapper.error(new NotFoundError('can not find user', []));
   }
 
+  users.data.map(data => {
+    const user = Model.user();
+    user.id = data.id;
+    user.name = data.name;
+    user.gender = data.gender;
+    user.createdAt = data.createdAt;
+    user.updatedAt = data.updatedAt;
+
+    result.push(user);
+  });
+
   const countUsers = await query.countUsers();
 
   metadata = {
     page,
-    size: users.data.length,
+    size: result.length,
     totalPage: Math.ceil(countUsers.data / size),
     totalData: countUsers.data
   };
 
-  return wrapper.paginationData(users.data, metadata);
+  return wrapper.paginationData(result, metadata);
 };
 
 module.exports = {
