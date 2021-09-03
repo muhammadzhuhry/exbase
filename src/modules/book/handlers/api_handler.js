82523ba18ab6d9c9d4c41344e05cd0a5e047ea40
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-09-03 23:41:04 +0700

    create handler get one book

--- src/modules/book/handlers/api_handler.js
+++ src/modules/book/handlers/api_handler.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const commandsDomain = require('../repositories/commands/domain');
+const queriesDomain = require('../repositories/queries/domain');
 const validator = require('../utils/validator');
 const wrapper = require('../../../helpers/utils/wrapper');
 const { SUCCESS:http } = require('../../../helpers/http-status/status-code');
@@ -7,6 +8,18 @@ const { SUCCESS:http } = require('../../../helpers/http-status/status-code');
 const router = express.Router();
 
 // QUERIES
+
+// handler get one book
+router.get('/:id', async (req, res) => {
+  const bookId = req.params.id;
+
+  const getOneBook = async () => queriesDomain.getOneBook(bookId);
+  const sendResponse = async (result) => {
+    (result.error) ? wrapper.response(res, 'fail', result, result.error.message, result.error.code)
+      : wrapper.response(res, 'success', result, `success get book with id ${bookId}`, http.OK);
+  };
+  sendResponse(await getOneBook());
+});
 
 // COMMANDS
 
