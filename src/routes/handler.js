d2322227ffe65c3a76bef92c2bedbdaca4f316e2
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-09-03 23:25:31 +0700

    create group route for module book

--- src/routes/handler.js
+++ src/routes/handler.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const userHandler = require('../modules/user/handlers/api_handler');
+const bookHandler = require('../modules/book/handlers/api_handler');
 
 const router = express.Router();
 
 // module user
 router.use('/user', userHandler);
+
+// module book
+router.use('/book', bookHandler);
 
 module.exports = router;
