e5c9037e147eee6ac58f3ff374cea8fa0f679190
Muhammad Athallah Zhuhry <muhammadzhuhry@gmail.com>
2021-09-04 18:44:18 +0700

    create feature update book

--- src/modules/book/repositories/commands/domain.js
+++ src/modules/book/repositories/commands/domain.js
@@ -1,13 +1,16 @@
 const dateFormat = require('dateformat');
+const validate = require('validate.js');
 const Command = require('./command');
+const Query = require('../queries/query');
 const Model = require('./command_model');
 const config = require('../../../../global_config');
 const Mysql = require('../../../../helpers/databases/mysql/db');
 const wrapper = require('../../../../helpers/utils/wrapper');
-const { InternalServerError } = require('../../../../helpers/error');
+const { InternalServerError, NotFoundError } = require('../../../../helpers/error');
 
 const mysql = new Mysql(config.get('/mysql'));
 const command = new Command(mysql);
+const query = new Query(mysql);
 
 const insertBook = async (data) => {
   let payload = { ...data };
@@ -27,6 +30,32 @@ const insertBook = async (data) => {
   return wrapper.data(book);
 };
 
+const updateBook = async (id, data) => {
+  let payload;
+  const bookId = parseInt(id);
+
+  const book = await query.findOneBook(bookId);
+  if (validate.isEmpty(book.data)) {
+    return wrapper.error(new NotFoundError(`can not find book with id ${bookId}`, {}));
+  }
+
+  payload = book.data[0];
+
+  payload.title = validate.isEmpty(data.title) ? payload.title : data.title;
+  payload.author = validate.isEmpty(data.author) ? payload.author : data.author;
+  payload.description = validate.isEmpty(data.description) ? payload.description : data.description;
+
+  payload.updatedAt  = dateFormat(new Date().toLocaleString(), 'yyyy-mm-dd HH:MM:ss');
+
+  const update = await command.updateBook(payload);
+  if (update.error) {
+    return wrapper.error(new InternalServerError(`failed update book with id ${bookId}`, {}));
+  }
+
+  return wrapper.data(payload);
+};
+
 module.exports = {
-  insertBook
+  insertBook,
+  updateBook
 };
