bd78fbc2a5d2ab2f2ca22dbd8b0d2add830e0490
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-01-06 14:39:54 +0700

    create query find user by email

--- src/modules/user/repositories/queries/query.js
+++ src/modules/user/repositories/queries/query.js
@@ -9,6 +9,13 @@ class Query {
     return recordset;
   }
 
+  async findUserByEmail(email) {
+    const valueData = [email];
+    const query = 'SELECT * FROM Users WHERE email = ?';
+    const result = await this.db.preparedQuery(query, valueData);
+    return result;
+  }
+
   async findUsers(fieldName, row, page, parameter) {
     this.db.setCollection('users');
     const recordset = await this.db.findMany(fieldName, row, page, parameter);
