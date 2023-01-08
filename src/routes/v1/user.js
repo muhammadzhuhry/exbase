291f4c82001aeb2d52cf1bf086421845cf51be6c
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-01-08 15:32:45 +0700

    use jwt auth on API get one user by email

--- src/routes/v1/user.js
+++ src/routes/v1/user.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const jwtAuth = require('../../auth/jwt_auth_helper');
 const basicAuth = require('../../auth/basic_auth_helper');
 const userHandler = require('../../modules/user/handlers/api_handler');
 
@@ -6,7 +7,7 @@ const router = express.Router();
 
 router.get('/root', basicAuth.isAuthenticated, userHandler.root);
 router.get('/', basicAuth.isAuthenticated, userHandler.getUsers);
-router.get('/:id', basicAuth.isAuthenticated, userHandler.getOneUser);
+router.get('/:email', jwtAuth.verifyToken, userHandler.getOneUser);
 router.post('/register', basicAuth.isAuthenticated, userHandler.registerUser);
 router.put('/update/:id', basicAuth.isAuthenticated, userHandler.updateUser);
 router.post('/login', basicAuth.isAuthenticated, userHandler.loginUser);
