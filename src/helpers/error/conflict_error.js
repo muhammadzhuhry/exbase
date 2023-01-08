a2d87a0be369a21f25a05821ddfc0ea8f3f23bdf
muhammadzhuhry <muhammadzhuhry@gmail.com>
2023-01-08 16:24:49 +0700

    add params data with default value of object

--- src/helpers/error/conflict_error.js
+++ src/helpers/error/conflict_error.js
@@ -1,7 +1,7 @@
 const { ERROR:httpError } = require('../http-status/status-code');
 
 class ConflictError {
-  constructor(param = 'conflict', data) {
+  constructor(param = 'conflict', data = {}) {
     this.message = param.message || param;
     this.data = data;
     this.code = httpError.CONFLICT;
